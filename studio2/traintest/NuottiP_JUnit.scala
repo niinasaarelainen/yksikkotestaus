8555bab1b0114581f21ab812d9731b0fef169106
Niina Saarelainen <nsaarela@iki.fi>
2017-03-02 01:28:45 +0200

    samassa testissä voi arvioida virheelliset ja oikeat syötteet 2

--- studio2/traintest/NuottiP_JUnit.scala
+++ studio2/traintest/NuottiP_JUnit.scala
@@ -1,7 +1,6 @@
 package studio2.traintest
 
 import scala.collection.mutable.Buffer
-import scala.collection.mutable.Map
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.FlatSpec
@@ -14,7 +13,7 @@ import studio2.train._
 class NuottiPTest extends FlatSpec with Matchers {
     
   
-  
+ // #1 
 "NuottiPiirturi" should "find non-valid note names (not investigating length)" in {
      val nuottejaVaarin = Buffer("t1-", "d3-", "f",  "p", "1c", "2f", "f2f2", "f2f", "f2f----", "hb22----", "c11", "d#b", "hb#", 
          "ddd", "d1d", "ab2", "a#2----", "hb2", "a2", "#c" )  // ,  "h2"
@@ -70,47 +69,7 @@ class NuottiPTest extends FlatSpec with Matchers {
    } 
   
 
-  
-  /* BU , ilman taukoja: 
-   
-   "NuottiPiirturi" should "find non-valid note names (not investigating length)" in {
-     val nuottejaV = Buffer("t1-", "d3-", "f",  "p", "1c", "2f", "f2f2", "f2f", "f2f----", "hb22----", "c11", "d#b", "hb#", 
-         "ddd", "d1d", "ab2", "a#2----", "hb2", "a2" )  // ,  "h2"
-    
-     val nuottejaO = Buffer("d1#.----", "e1----.#", "c1-.", "g1-", "H#2", "Gb1----", "Ab1--",  "f#1---", "d2b", "a-----1#")  // 
-    
-    var virheita = 0 
-    for (nuotti <- nuottejaO) {
-       
-        val filtteredNote = nuotti.filter(_ != '-').filter(_ != '.')
-   //     println(filtteredNote)
-         
-        if(!"cdefgah".contains(filtteredNote.toLowerCase().head.toString()))
-            virheita += 1
-        else if(!(filtteredNote.tail.contains("1")|| filtteredNote.tail.contains("2")))   
-          virheita += 1  
-        else if(filtteredNote.size < 2)
-          virheita += 1
-        else if(filtteredNote.size > 3)
-          virheita += 1  
-        else if(filtteredNote.tail.contains("#b") ||   filtteredNote.tail.contains("#b"))    
-            virheita += 1 
-        else if  (filtteredNote.contains("a") &&  filtteredNote.contains("2"))  // löytää a2, a#2, ab2 = piirtoalueen ulkopuolella    
-                    virheita += 1  
-            
-        else if(filtteredNote.contains("h") &&   filtteredNote.contains("2"))  // löytää h2, h#2, hb2 = piirtoalueen ulkopuolella    
-            virheita += 1       
-             
-        else if(filtteredNote.size == 3){   // tämä testi vikana !!!
-            if(!(filtteredNote.tail.contains("#") || filtteredNote.tail.contains("b")))   
-             virheita += 1  
-        }     
-    }  
-      assert(virheita == nuottejaO.size)   
-   } 
-  
- */
-   
+// #2   
 "NuottiPiirturi" should "find non-valid lengths)" in {
      val pituuksiaVaarin = Buffer("d1#.----", "f2-----", "g1.", "h1---------", "a1---."  )
      val pituuksiaOikein = Buffer( "c1", "c1-", "c1-.", "e1--#",  "H#2--.", "Gb1---", "Ab1----")  // kaikki sallitut pituudet
@@ -141,6 +100,4 @@ class NuottiPTest extends FlatSpec with Matchers {
     
       assert(virheitaHyvaksyttavillaPituuksilla == 0  &&  virheitaHylattavillaPituuksilla == pituuksiaVaarin.size )   
    } 
-
-  
 }
