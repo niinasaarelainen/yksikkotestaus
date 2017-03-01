3afcaec924ca7e2f53f7aa53462935bb0419c967
Niina Saarelainen <nsaarela@iki.fi>
2017-03-02 01:24:35 +0200

    samassa testissä voi arvioida virheelliset ja oikeat syötteet

--- studio2/traintest/NuottiP_JUnit.scala
+++ studio2/traintest/NuottiP_JUnit.scala
@@ -14,48 +14,59 @@ import studio2.train._
 class NuottiPTest extends FlatSpec with Matchers {
     
   
-  // Fill in the tests below. The first one is given as an example. 
   
-   "NuottiPiirturi" should "find non-valid note names (not investigating length)" in {
-     val nuottejaV = Buffer("t1-", "d3-", "f",  "p", "1c", "2f", "f2f2", "f2f", "f2f----", "hb22----", "c11", "d#b", "hb#", 
+"NuottiPiirturi" should "find non-valid note names (not investigating length)" in {
+     val nuottejaVaarin = Buffer("t1-", "d3-", "f",  "p", "1c", "2f", "f2f2", "f2f", "f2f----", "hb22----", "c11", "d#b", "hb#", 
          "ddd", "d1d", "ab2", "a#2----", "hb2", "a2", "#c" )  // ,  "h2"
-     val taukojaV= Buffer("za", "z#--", "zz", "zz-top", "tauko")     
+     val taukojaVaarin= Buffer("za", "z#--", "zz", "zz-top", "tauko", " z")     
     
-     val nuottejaO = Buffer("d1#.----", "e1----.#", "c1-.", "g1-", "H#2", "Gb1----", "Ab1--",  "f#1---", "d2b", "a-----1#",
+     val nuottejaOikein = Buffer("d1#.----", "e1----.#", "c1-.", "g1-", "H#2", "Gb1----", "Ab1--",  "f#1---", "d2b", "a-----1#",
          "--c1", "----g2")  // 
-     val taukojaO= Buffer("z", "z--", "z----", "z.", "-z")   
+     val taukojaOikein= Buffer("z", "z--", "z----", "z.", "-z", "z--.", "--.z")   
      
-     var virheita = 0 
+   
+     var virheitaHylattavillaNuoteilla, virheitaHyvaksyttavillaNuoteilla, virheitaHylattavillaTauoilla, virheitaHyvaksyttavillaTauoilla = 0
     
-     for (nuotti <- nuottejaO) {
+     virheitaHylattavillaNuoteilla = laskeVirheet( nuottejaVaarin)
+     virheitaHyvaksyttavillaNuoteilla =  laskeVirheet( nuottejaOikein)
+     virheitaHylattavillaTauoilla = laskeVirheet( taukojaVaarin)
+     virheitaHyvaksyttavillaTauoilla = laskeVirheet( taukojaOikein)
+     
+   def laskeVirheet(syotteet: Buffer[String]) = {   
+      var virheita = 0 
+      for (syote <- syotteet) {
        
-        val filtteredNote = nuotti.filter(_ != '-').filter(_ != '.')
-   //     println(filtteredNote)
+         val filtteredNote = syote.filter(_ != '-').filter(_ != '.')
          
-        if(filtteredNote == "z")
-          {}
-        else{
-          if(!"cdefgah".contains(filtteredNote.toLowerCase().head.toString()))
-              virheita += 1
-          else if(!(filtteredNote.tail.contains("1")|| filtteredNote.tail.contains("2")))   
-            virheita += 1  
-          else if(filtteredNote.size < 2)
-            virheita += 1
-          else if(filtteredNote.size > 3)
-            virheita += 1  
-          else if(filtteredNote.tail.contains("#b") ||   filtteredNote.tail.contains("#b"))    
-              virheita += 1 
-          else if  (filtteredNote.contains("a") &&  filtteredNote.contains("2"))  // löytää a2, a#2, ab2 = piirtoalueen ulkopuolella    
-              virheita += 1      
-          else if(filtteredNote.contains("h") &&   filtteredNote.contains("2"))  // löytää h2, h#2, hb2 = piirtoalueen ulkopuolella    
-              virheita += 1               
-          else if(filtteredNote.size == 3){   // tämä testi vikana !!!
-              if(!(filtteredNote.tail.contains("#") || filtteredNote.tail.contains("b")))   
-                 virheita += 1  
-          }    
-        } // iso else
-    }  // for
-      assert(virheita == nuottejaO.size)   
+         if(filtteredNote == "z")
+            {}
+         else{
+            if(!"cdefgah".contains(filtteredNote.toLowerCase().head.toString()))
+               virheita += 1
+            else if(!(filtteredNote.tail.contains("1")|| filtteredNote.tail.contains("2")))   
+               virheita += 1  
+            else if(filtteredNote.size < 2)
+               virheita += 1
+            else if(filtteredNote.size > 3)
+               virheita += 1  
+            else if(filtteredNote.tail.contains("#b") ||  filtteredNote.tail.contains("#b"))    
+               virheita += 1 
+            else if  (filtteredNote.contains("a") &&  filtteredNote.contains("2"))  // löytää a2, a#2, ab2 = piirtoalueen ulkopuolella    
+               virheita += 1      
+            else if(filtteredNote.contains("h") &&   filtteredNote.contains("2"))  // löytää h2, h#2, hb2 = piirtoalueen ulkopuolella    
+               virheita += 1               
+            else if(filtteredNote.size == 3){   // tämä testi vikana !!!
+               if(!(filtteredNote.tail.contains("#") || filtteredNote.tail.contains("b")))   
+                   virheita += 1  
+            }    
+          } // iso else
+      }  // for
+      
+      virheita
+}
+     
+      assert(virheitaHyvaksyttavillaNuoteilla == 0  &&  virheitaHylattavillaNuoteilla == nuottejaVaarin.size && 
+          virheitaHyvaksyttavillaTauoilla == 0 &&  virheitaHylattavillaTauoilla == taukojaVaarin.size)  
    } 
   
 
@@ -100,24 +111,35 @@ class NuottiPTest extends FlatSpec with Matchers {
   
  */
    
-    "NuottiPiirturi" should "find non-valid length)" in {
-     val nuottejaV = Buffer("d1#.----", "f2-----", "g1.", "h1---------", "a1---."  )
-     val nuottejaO = Buffer( "c1", "c1-", "c1-.", "e1--#",  "H#2--.", "Gb1---", "Ab1----")  // kaikki sallitut pituudet
+"NuottiPiirturi" should "find non-valid lengths)" in {
+     val pituuksiaVaarin = Buffer("d1#.----", "f2-----", "g1.", "h1---------", "a1---."  )
+     val pituuksiaOikein = Buffer( "c1", "c1-", "c1-.", "e1--#",  "H#2--.", "Gb1---", "Ab1----")  // kaikki sallitut pituudet
     
-    var virheita = 0 
-    for (nuotti <- nuottejaO) {
-        val lkm = nuotti.count(_ == '-')
+     var virheitaHylattavillaPituuksilla, virheitaHyvaksyttavillaPituuksilla = 0
+     
+     virheitaHylattavillaPituuksilla = laskeVirheet( pituuksiaVaarin)
+     virheitaHyvaksyttavillaPituuksilla =  laskeVirheet( pituuksiaOikein)
+    
+     
+   def laskeVirheet(syotteet: Buffer[String] ) = {
+     var virheita = 0 
+     for (syote <- syotteet) {
+        val lkm = syote.count(_ == '-')
         println(lkm)
         if(lkm > 4)
             virheita += 1
-        else if(lkm == 3 && nuotti.contains("."))    // ohjelmassa ei määritelty pisteellistä pisteellistä puolinuottia
+        else if(lkm == 3 && syote.contains("."))    // ohjelmassa ei määritelty pisteellistä pisteellistä puolinuottia
           virheita += 1   
-        else if(lkm == 4 && nuotti.contains("."))   // max pituus 4
+        else if(lkm == 4 && syote.contains("."))   // max pituus 4
           virheita += 1  
-        else if(lkm == 0 && nuotti.contains("."))    // ei pisteellistä kahdeksasosaa
+        else if(lkm == 0 && syote.contains("."))    // ei pisteellistä kahdeksasosaa
           virheita += 1     
-      }     
-      assert(virheita == nuottejaO.size)   
+     }     
+    
+     virheita
+   }  
+    
+      assert(virheitaHyvaksyttavillaPituuksilla == 0  &&  virheitaHylattavillaPituuksilla == pituuksiaVaarin.size )   
    } 
 
   
