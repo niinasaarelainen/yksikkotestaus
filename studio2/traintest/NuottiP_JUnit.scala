a2a5738754f6c80853cf364d4e8e1afe13b7cd39
Niina Saarelainen <nsaarela@iki.fi>
2017-03-13 08:45:54 +0200

    uusien testien nimiä

--- studio2/traintest/NuottiP_JUnit.scala
+++ studio2/traintest/NuottiP_JUnit.scala
@@ -17,14 +17,14 @@ class NuottiPTest extends FlatSpec with Matchers {
 "NuottiPiirturi" should "find non-valid note names (not investigating length)" in {
      
      val nuottejaOikein = Buffer("d1#.----", "e1----.#", "c1-.", "g1-", "H#2", "Gb1----", "Ab1--",  "f#1---", "d2b", "a-----1#",
-         "--c1", "----g2", "ab2", "a#2----")  // 
+         "--c1", "----g2", "ab2", "a#2----", "b1", "bb1", "b#1", "b2", "bb2", "b#2")  // 
   
-     val nuottejaVaarin = Buffer("t1-", "d3-", "f",  "p", "1c", "2f", "f2f2", "f2f", "f2f----", "hb22----", "c11", "d#b", "hb#", 
-         "ddd", "d1d",  "hb2", "h2", "#c" ) 
+     val nuottejaVaarin = Buffer("t1-", "d3-", "f",  "p", "1c", "2f", "f2f2", "f2f", "f2g----", "hb22----", "c11", "d#b", "b#", 
+         "ddd", "d1d",  "#c", "dd1", "g##", "abb", "dis" ) 
      
-     val taukojaOikein= Buffer("z", "z--", "z----", "z.", "-z", "z--.", "--.z")   
+     val taukojaOikein= Buffer("z", "z--", "z----", "z.", "-z", "z--.", "--.z", "z.-")   
      
-     val taukojaVaarin= Buffer("za", "z#--", "zz", "zz-top", "tauko", " z")     
+     val taukojaVaarin= Buffer("za", "z#--", "zz", "zz-top", "tauko", " z", "az")     
     
    
      var virheitaHylattavillaNuoteilla, virheitaHyvaksyttavillaNuoteilla, virheitaHylattavillaTauoilla, virheitaHyvaksyttavillaTauoilla = 0
@@ -43,7 +43,7 @@ class NuottiPTest extends FlatSpec with Matchers {
          if(filtteredNote == "z")
             {}
          else{
-            if(!"cdefgah".contains(filtteredNote.toLowerCase().head.toString()))
+            if(!"cdefgahb".contains(filtteredNote.toLowerCase().head.toString()))
                virheita += 1
             else if(!(filtteredNote.tail.contains("1")|| filtteredNote.tail.contains("2")))   
                virheita += 1  
@@ -51,27 +51,23 @@ class NuottiPTest extends FlatSpec with Matchers {
                virheita += 1
             else if(filtteredNote.size > 3)
                virheita += 1  
-            else if(filtteredNote.tail.contains("#b") ||  filtteredNote.tail.contains("#b"))    
+            else if(filtteredNote.tail.contains("#b") ||  filtteredNote.tail.contains("b#"))    
                virheita += 1 
-            else if(filtteredNote.contains("h") &&   filtteredNote.contains("2"))  // löytää h2, h#2, hb2 = piirtoalueen ulkopuolella    
-               virheita += 1               
-            else if(filtteredNote.size == 3){   // tämä testi vikana !!!
-               if(!(filtteredNote.tail.contains("#") || filtteredNote.tail.contains("b")))   
-                   virheita += 1  
-            }    
+            else if(filtteredNote.size == 3 && !(filtteredNote.tail.contains("#") || filtteredNote.tail.contains("b")))   
+                     virheita += 1  
+               
           } // iso else
       }  // for
       
       virheita
-}
-     
+}     
       assert(virheitaHyvaksyttavillaNuoteilla == 0  &&  virheitaHylattavillaNuoteilla == nuottejaVaarin.size && 
           virheitaHyvaksyttavillaTauoilla == 0 &&  virheitaHylattavillaTauoilla == taukojaVaarin.size)  
    } 
   
 
 // #2   
-"NuottiPiirturi" should "find non-valid lengths)" in {
+"NuottiPiirturi" should "find non-valid lengths" in {
      val pituuksiaVaarin = Buffer("d1#.----", "f2-----", "g1.", "h1---------", "a1---."  )
      val pituuksiaOikein = Buffer( "c1", "c1-", "c1-.", "e1--#",  "H#2--.", "Gb1---", "Ab1----")  // kaikki sallitut pituudet
     
@@ -101,4 +97,29 @@ class NuottiPTest extends FlatSpec with Matchers {
     
       assert(virheitaHyvaksyttavillaPituuksilla == 0  &&  virheitaHylattavillaPituuksilla == pituuksiaVaarin.size )   
    } 
+
+// #3   
+"NuottiPiirturi" should "draw half note stem down" in {
+  
+ //  assert()
+}
+
+// #   
+"NuottiPiirturi" should "draw eight note couple right" in {
+  
+ //  assert()
+}
+
+// #  
+"NuottiPiirturi" should "draw chords right" in {
+  
+ //  assert()
+}
+
+// #
+"NuottiPiirturi" should "exit with empty file and with empty note data" in {
+  
+ //  assert()
+}
+
 }
